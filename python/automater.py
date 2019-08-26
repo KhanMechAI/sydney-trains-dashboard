77e01bf1dd385e5de65625b5f1534dad52f9a31e
KhanMechAI <37394116+KhanMechAI@users.noreply.github.com>
2019-08-26 18:11:12 +1000

    many bug fixes. Functional rn.

--- python/automater.py
+++ python/automater.py
@@ -4,16 +4,22 @@ import pandas as pd
 import numpy as np
 import xlsxwriter
 
+def _cm_to_inch(length):
+    return np.divide(length,2.54)
 
 #Directory constants
+ISSUE = 3
 MONTH = 'July 2019'
 SUB_DIR = 'Job Managers'
 DASH = 'Dashboard'
 DASHBOARD_DIRECTORY = r"C:\Users\kschroder-turner\Documents\TEMP\Monthly Dashboards"
 XLSX = '.xlsx'
 TMP_FILE = "~$"
+GHD_LOGO = r'C:\Users\kschroder-turner\Documents\TEMP\tmp\logo\ghd_logo.png'
+ST_LOGO = r'C:\Users\kschroder-turner\Documents\TEMP\tmp\logo\st_logo.png'
 
 #Dataframe constants
+TASK_CODE = "Task Code" #BST constant
 JOB_NUM = 'GHD Job Number'
 C_C_DATE = "Contractual Completion Date"
 CUR_STAT = 'Current Status'
@@ -25,6 +31,8 @@ PROJECT = "Project Name"
 ST_DES_MAN = 'ST Design Manager'
 ST_REF_PO = 'ST Reference No. / Purchase Order Number'
 SCH = 'Schedule'
+COMMENTS = 'Comments'
+ACTION_BY = 'Action By'
 COL_ORDER = [
     ST_REF_PO,
     PROJECT,
@@ -35,7 +43,9 @@ COL_ORDER = [
     C_C_DATE, 
     F_C_DATE,
     CUR_STAT, 
-    NEXT_ACTION, 
+    NEXT_ACTION,
+    ACTION_BY,
+    COMMENTS,
 ]
 BST_COLS = [
     ST_REF_PO,
@@ -52,14 +62,22 @@ BST_RAW_COLS = [
     "Project Code",
     "Project Manager Name",
     "Project Name",
-    "Billable",
+    TASK_CODE,
 ]
 HEADERS = [JOB_NUM, *COL_ORDER]
+
+COL_WIDTH = [14,15,14,13,14,9,9,14,14,35,31,16,80]
 
 DATE_FORMAT = '%d-%m-%Y'
 DATETIME_TYPE_STRING = 'datetime64[ns]'
 
 # XLSXWRITER constants
+MARGINS = {
+    'left':_cm_to_inch(0.6),
+    'right':_cm_to_inch(0.6),
+    'top':_cm_to_inch(3),
+    'bottom':_cm_to_inch(1.9),
+}
 SHEET1_NAME = 'ST Dashboard'\
 ## Colours
 GHD_BLUE = '#006DA3'
@@ -71,31 +89,44 @@ BEHIND_SCHEDULE_CELL_FILL = '#ffc7ce'
 AT_RISK_CELL_FILL = '#ffeb9c'
 ON_TRACK_CELL_FILL = '#c6efce'
 ## Formats
-HEADER_FORMAT = {
+BASE_FORMAT = {
     'bold': False,
     'text_wrap': True,
     'valign': 'vcenter',
     'align': 'center',
-    'bg_color': GHD_BLUE,
     'border': 1,
-    'border_color': WHITE,
     'font_name': 'arial',
-    'font_color': WHITE,
     'font_size': 11,
+    'locked': 0,
+}
+HEADER_FORMAT = {
+    'bg_color': GHD_BLUE,
+    'border_color': WHITE,
+    'font_color': WHITE,
 }
 BEHIND_SCHEDULE_FORMAT = {
-    'bold': False,
-    'text_wrap': True,
-    'valign': 'vcenter',
-    'align': 'center',
-    'bg_color': GHD_BLUE,
-    'border': 1,
-    'border_color': WHITE,
-    'font_name': 'arial',
-    'font_color': WHITE,
-    'font_size': 11,
+    'bg_color': BEHIND_SCHEDULE_CELL_FILL,
+    'font_color': BEHIND_SCHEDULE_TEXT_COLOUR,
 }
-COL_WIDTH = 25
+AT_RISK_FORMAT = {
+    'bg_color': AT_RISK_CELL_FILL,
+    'font_color': AT_RISK_TEXT_COLOUR,
+}
+ON_TRACK_FORMAT = {
+    'bg_color': ON_TRACK_CELL_FILL,
+    'font_color': ON_TRACK_TEXT_COLOUR,
+}
+NEW_JOB_FORMAT = {
+    'bold': True,
+    'border': 12,
+}
+
+NEW_PM_FORMAT = {
+    'bold': True,
+    'border': 13,
+}
+
+
 PHASE_D_VAL = {
     'validate': 'list',
     'source': [
@@ -126,7 +157,15 @@ LOCKED_FMT = {
     'locked': 0,
 }
 
-
+EXCLUSIONS = {
+    JOB_NUM: [
+        2127653,
+    ],
+    PM: [
+        'Winston Wang',
+        'Ruevern Barritt',
+    ]
+}
 
 #TODO: Make .exe version
 #TODO: Fix the path and directory bug. Access the sharepoint directly.
@@ -141,18 +180,25 @@ def main():
     writer = pd.ExcelWriter(Path(DASHBOARD_DIRECTORY) / 'OUTPUT.xlsx', engine='xlsxwriter')
     [columns, master_df] = _get_master(MONTH)
     bst_df = _get_bst(0, sheet=0)
-    print(bst_df.head())
-    master_df = _update_bst(master_df, bst_df)
-    updated_master_df = _copy_to_master(master_df, columns, MONTH)
-    _export_master_sheet(updated_master_df, writer)
-    _export_pm_sheets(updated_master_df)
+    master_df, new_data = _update_bst(master_df, bst_df)
+    master_df = _copy_to_master(master_df, columns, MONTH)
+    master_df =_exclude(master_df, EXCLUSIONS)
+    _export_master_sheet(master_df, writer, new_data)
+    _export_pm_sheets(master_df)
     return
+
+def _exclude(df, exclusions):
+    for key, val in exclusions.items():
+        if key == JOB_NUM:
+            continue
+        df = df[~df[key].isin(val)]
+    return df
 
 def _get_bst(fileId, sheet=0):
     fileId = Path(r"C:\Users\kschroder-turner\Documents\TEMP\tmp\BST10 Output.xlsx")
     df = pd.read_excel(fileId, sheet_name=sheet, index_col=0, usecols=BST_RAW_COLS)
-    df = df[df.Billable == True]
-    df.drop(["Billable"], inplace=True, axis=1) 
+    df = df[df[TASK_CODE] != "PP"]
+    df.drop([TASK_CODE], inplace=True, axis=1) 
     df = _handle_index(df)
     df.columns = [PROJECT, PM]
     return df
@@ -168,7 +214,13 @@ def _update_bst(master, bst):
     idx1 = bst.index
     idx2 = master.index
     idx_diff = idx1.difference(idx2)
-    return master.append(bst.loc[idx_diff])
+    bst_pm = set(bst[PM].unique())
+    master_pm = set(bst[PM].unique())
+    new_data = {
+        'pm':bst_pm.difference(master_pm),
+        'jobs':idx_diff,
+    }
+    return master.append(bst.loc[idx_diff]), new_data
 
 def _export_pm_sheets(df):
     _all_pms = df[PM].unique()
@@ -179,15 +231,13 @@ def _export_pm_sheets(df):
     for name in _all_pms: 
         _name = _output_dir / Path(str(name)+XLSX) #Generate file path and name for PM
         _df = df[df[PM]==name] #Extract PM data from main dataframe
-        _wr, _wb, _ws = _excel_setup(_name, SHEET1_NAME)
-        _ul = _wb.add_format(LOCKED_FMT) #Get the unlocked cell format
-        header_format = _wb.add_format(HEADER_FORMAT) #Specify the header format
+        _wr, _wb, _ws = _excel_setup(_name, SHEET1_NAME)#Specify the header format
         _ws.protect() #Lock all the cells
-        _header_format(_ws, header_format) #Format the header cells
+        _header_format(_wb, _ws) #Format the header cells
         _cell_range = _editable_cell_range(_df) #Get the range of editable cells
         _data_validation(_ws, _df, PHASE, PHASE_D_VAL)#Set up data validation
         _data_validation(_ws, _df, SCH, SCHEDULE_D_VAL) 
-        _format_cells(_ws, _ul, _cell_range, df=_df) #Unlock the desired range of editable cells and paste in data
+        _format_cells(_wb, _ws, _cell_range, df=_df) #Unlock the desired range of editable cells and paste in data
         _wr.save()#Save the workbook
     return
 
@@ -196,6 +246,27 @@ def _excel_setup(file_path, sheet_name):
     workbook = writer.book 
     worksheet = workbook.add_worksheet(sheet_name)#Add a named worksheet to the workbook
     return writer, workbook, worksheet
+
+def _sheet_setup(worksheet):
+    worksheet.set_paper(8)
+    worksheet.set_page_view()
+    worksheet.set_landscape()
+    worksheet.set_header(
+        f'&L&[Picture]&C&14&"Arial,Bold"GHD Monthly Dashboard\nIssue {ISSUE}: ({MONTH})&R&[Picture]', 
+        {
+            'image_left': GHD_LOGO,
+            'image_right': ST_LOGO,
+            }
+    )
+    worksheet.set_footer('&CPage &P of &N')
+    worksheet.set_margins(
+        left=MARGINS['left'],
+        right=MARGINS['right'],
+        top=MARGINS['top'],
+        bottom=MARGINS['bottom'],
+    )
+    worksheet.repeat_rows(0)
+    return
 
 def _get_col_idx(df, col):
     return df.columns.get_loc(col)
@@ -210,22 +281,40 @@ def _data_validation(sheet, df, col, val_fmt):
 def _editable_cell_range(df):
     return [1, df.shape[0]+1, 0, df.shape[1]+1]
 
-def _format_cells(sheet, cell_format, cell_range, df=pd.DataFrame(), value=None):
+def _format_cells(workbook, sheet, cell_range, df=pd.DataFrame(), value=None, new_jobs=[], new_pms=[]):
     _row_start, _row_finish, _col_start, _col_finish = cell_range
+    
+    def _get_format():
+        cell_format = BASE_FORMAT
+        if df.index.values[row-1] in new_jobs:
+            cell_format = {**cell_format, **NEW_JOB_FORMAT}
+        if df.iloc[row-1, 2] in new_pms:
+            cell_format = {**cell_format, **NEW_PM_FORMAT}
+        schedule = _check_not_nan(df.iloc[row-1, 5])
+        if schedule:
+            if schedule.lower() ==  SCHEDULE_D_VAL['source'][0].lower():
+                cell_format = {**cell_format, **ON_TRACK_FORMAT}
+            elif schedule.lower() ==  SCHEDULE_D_VAL['source'][1].lower():
+                cell_format = {**cell_format, **AT_RISK_FORMAT}
+            elif schedule.lower() ==  SCHEDULE_D_VAL['source'][2].lower():
+                cell_format = {**cell_format, **BEHIND_SCHEDULE_FORMAT}
+        return workbook.add_format(cell_format)
 
     if not value:
         for row in range(_row_start, _row_finish):
+            cell_format = _get_format()
             for col in range(_col_start, _col_finish):
                 sheet.write(row, col, value, cell_format)
-    
+
     if not df.empty:
         for row in range(_row_start, _row_finish):
+            cell_format = _get_format()
             for col in range(_col_start, _col_finish):
                 if col == 0:
                     value = df.index.values[row-1]
                 else:
                     value = _check_not_nan(df.iloc[row-1, col-1])
-                sheet.write(row, col, value, cell_format)           
+                sheet.write(row, col, value, cell_format)          
     
 def _check_not_nan(value):
     if not value:
@@ -237,12 +326,14 @@ def _check_not_nan(value):
     else:
         return value
 
-def _header_format(sheet, header_format):
+def _header_format(workbook, sheet):
+    header_format = {**BASE_FORMAT, **HEADER_FORMAT}
+    header_format = workbook.add_format(header_format)
     for col_num, value in enumerate(HEADERS):
         sheet.write(0, col_num, value, header_format)
-        sheet.set_column(col_num, col_num, COL_WIDTH)
+        sheet.set_column(col_num, col_num, COL_WIDTH[col_num])
 
-def _export_master_sheet(df, writer):
+def _export_master_sheet(df, writer,new_data):
     """This function outputs the final dashboard sheet. It writes all the PM sheets for the following month too.
     
     Arguments:
@@ -250,11 +341,14 @@ def _export_master_sheet(df, writer):
         writer {xlsxwriter} -- The handle to the xlsxwriter for the excel sheet
     """
     df.to_excel(writer, sheet_name=SHEET1_NAME, startrow=1, header=False)
-    workbook = writer.book
-    worksheet = writer.sheets[SHEET1_NAME]
-    print(df.head())
-    header_format = workbook.add_format(HEADER_FORMAT)
-    _header_format(worksheet, header_format)
+    _wb = writer.book
+    _ws = writer.sheets[SHEET1_NAME]
+    _sheet_setup(_ws)
+    _header_format(_wb, _ws)
+    _cell_range = _editable_cell_range(df) #Get the range of editable cells
+    _data_validation(_ws, df, PHASE, PHASE_D_VAL)#Set up data validation
+    _data_validation(_ws, df, SCH, SCHEDULE_D_VAL) 
+    _format_cells(_wb, _ws, _cell_range, df=df, new_jobs=new_data['jobs'], new_pms=new_data['pm'])
     writer.save()
     return
 
@@ -273,13 +367,20 @@ def _get_master(month):
     columns = []
     for f_name in os.listdir(_month_dir):
         if XLSX in f_name and DASH in f_name and TMP_FILE not in f_name:
-            df = pd.read_excel(_month_dir / f_name, index_col=1)
-            df.index.astype(int, copy=False)
+            df = pd.read_excel(_month_dir / f_name, index_col=0)
+    df = _handle_index(df)
     df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
     df.sort_index(inplace=True)
     df.sort_index(axis=1, inplace=True)
+    df = _add_missing_col(df)
     columns = df.columns
     return [columns, df]
+
+def _add_missing_col(df):
+    for col in COL_ORDER:
+        if col not in df.columns:
+            df[col] = ""
+    return df
 
 def _date_time_handler(df):
     if not df[C_C_DATE].isnull().values.all():
@@ -298,7 +399,7 @@ def _copy_to_master(master, columns, month):
         file = _jm_sheets / f_name
         if XLSX in f_name and TMP_FILE not in f_name:
             df = pd.read_excel(file, index_col=0)
-            df = df[columns]
+            df = df.iloc[:, :10]
             df = _handle_index(df)
             df.sort_index(axis=1, inplace=True)
             df = _date_time_handler(df)
